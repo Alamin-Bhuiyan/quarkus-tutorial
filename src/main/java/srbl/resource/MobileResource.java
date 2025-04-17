05ecce37be970725a3798fcc5cd83aa8b327c404
Alamin <amialaminb@gmail.com>
2025-04-17 19:39:00 +0600

    commit

---
+++ src/main/java/srbl/resource/MobileResource.java
@@ -0,0 +1,61 @@
+package srbl.resource;
+
+
+import jakarta.ws.rs.*;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Path("/mobile")
+public class MobileResource {
+    List<String> mobileList = new ArrayList<>();
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public Response getMobileList() {
+        return Response.ok(mobileList).build();
+    }
+
+    @POST
+    @Consumes(MediaType.TEXT_PLAIN)
+    @Produces(MediaType.TEXT_PLAIN)
+    public Response addMobile(String mobileName) {
+        mobileList.add(mobileName);
+        return Response.ok(mobileName).build();
+    }
+
+    @PUT
+    @Path("/{oldmobilename}")
+    @Consumes(MediaType.TEXT_PLAIN)
+    @Produces(MediaType.TEXT_PLAIN)
+    public Response updateMobile(@PathParam("oldmobilename") String oldMobileName,
+                                 @QueryParam("newmobilename") String newMobileName)
+    {
+        mobileList = mobileList.stream().map(mobile -> {
+            if(mobile.equals(oldMobileName)) {
+                return newMobileName;
+            }
+            else{
+                return mobile;
+            }
+        }).collect(Collectors.toList());
+        return Response.ok(mobileList).build();
+    }
+
+    @DELETE
+    @Path("{mobileToDelete}")
+    @Produces(MediaType.TEXT_PLAIN)
+    public  Response deleteMobile(@PathParam("mobileToDelete") String mobileToDelete)
+    {
+        boolean isRemoved = mobileList.remove(mobileToDelete);
+        if(isRemoved) {
+            return Response.ok(mobileList).build();
+        }
+        else {
+            return Response.status(Response.Status.NOT_FOUND).build();
+        }
+    }
+}
